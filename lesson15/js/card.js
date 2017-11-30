02e4a8dfe31ca17697e3aadd57b4cf58df4d7a96
Yespanic <yespanic@gmail.com>
2017-11-30 13:24:43 +0300

    simple denomination and suit

--- lesson15/js/card.js
+++ lesson15/js/card.js
@@ -1,12 +1,14 @@
-var denomination = function(){
-	for (var i=0; i<denomArr.length; i++) {
+var cardDenomination;
+var cardSuit;
+
+var denomination = function(){	for (var i=0; i<denomArr.length; i++) {
 		$('#denomination').append('<span class="button" data-number="'+i+'">' + denomArr[i] + '</span>');	
 	}	
 };
 
 var suit = function(){
 	for (var i=0; i<suitArr.length; i++) {
-		$('#suit').append('<span class="button"><img src="' + suitArr[i] + '">	</span>');
+		$('#suit').append('<span class="button" data-number="'+i+'"><img src="' + suitArr[i] + '">	</span>');
 	}
 };
 
@@ -18,5 +20,29 @@ suit();
 $('#denomination').on('click', '.button', function(){
 	$('#denomination .button').removeClass('active');
 	$(this).addClass('active');
-	localStorage.setItem('denomination', this);
+	cardDenomination = $(this).data('number');
+	console.log(denomArr[cardDenomination]);
+	updateCard();
+	// var object = $(this);
+	// var serialObject = JSON.stringify(object);
+	// localStorage.setItem("need", serialObject);
+	// var returnObject = JSON.parse(localStorage.getItem("need"));
 });
+
+$('#suit').on('click', '.button', function(){
+	$('#suit .button').removeClass('active');
+	$(this).addClass('active');
+	cardSuit = $(this).data('number');
+	console.log(suitArr[cardSuit]);
+	updateCard();
+});
+
+function updateCard() {
+	if (typeof(cardDenomination)==='undefined') return false; 
+	if (typeof(cardSuit)==='undefined') return false;
+	$('#card').html('<span>' + denomArr[cardDenomination] + '</span>' + 
+	'<span> <img src="' + suitArr[cardSuit] + '"> </span>');
+};
+
+
+
